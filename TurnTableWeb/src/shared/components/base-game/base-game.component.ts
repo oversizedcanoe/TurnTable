feaab6537b396efcd810e094b6bf39baa4d025c1
billy <billy.cook12345@gmail.com>
2024-12-19 21:13:22 -0500

    Got URL linking working and layout improved.

--- TurnTableWeb/src/shared/components/base-game/base-game.component.ts
+++ TurnTableWeb/src/shared/components/base-game/base-game.component.ts
@@ -3,6 +3,7 @@ import { GameService } from '../../../features/games/games.service';
 import { GameType } from '../../models/enums';
 import { StorageService } from '../../services/storage.service';
 import { PlayerDTO } from '../../models/models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-base-game',
@@ -15,18 +16,26 @@ export class BaseGameComponent implements OnInit {
   public title: string = '';
   public showLogin: boolean = false;
   public showGameDetails: boolean = false;
+  public gameCodeLink: string = '';
+  public gameCodeButtonText: string = '';
 
   @Input() gameService!: GameService;
   @Output() onNewGame = new EventEmitter<void>();
   @Output() onJoinGame = new EventEmitter<void>();
 
-  constructor(private storage: StorageService) {
+  constructor(private storage: StorageService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.playerName = this.gameService.playerName ?? '';
     this.showLogin = this.gameService.isGameMultiplayer();
     this.title = GameType[this.gameService.gameType ?? -1];
+
+    const gameCodeQueryParam: string | null = this.route.snapshot.queryParamMap.get('gameCode');
+
+    if (gameCodeQueryParam) {
+      this.gameCodeToJoin = gameCodeQueryParam;
+    }
   }
 
   async onStartClicked() {
@@ -34,7 +43,7 @@ export class BaseGameComponent implements OnInit {
     this.showLogin = false;
 
     if (this.gameService.isGameMultiplayer()) {
-      this.showGameDetails = true;
+      this.showGameDetailsSection();
     }
 
     this.onNewGame.next();
@@ -51,14 +60,20 @@ export class BaseGameComponent implements OnInit {
     this.showLogin = false;
 
     if (this.gameService.isGameMultiplayer()) {
-      this.showGameDetails = true;
+      this.showGameDetailsSection();
     }
 
     this.onJoinGame.next();
   }
 
+  showGameDetailsSection(): void {
+    this.showGameDetails = true;
+    this.gameCodeLink = window.location.origin + window.location.pathname + "?gameCode=" + this.gameService.gameCode;
+    this.gameCodeButtonText = this.gameService.gameCode + ' ✂';
+  }
+
   isButtonDisabled_Start() {
-    return this.playerName == '';
+    return this.playerName == '' || this.gameCodeToJoin != '';
   }
 
   isButtonDisabled_Join() {
@@ -83,7 +98,36 @@ export class BaseGameComponent implements OnInit {
     return this.gameService.gameState?.players ?? [];
   }
 
-  getGameCode(): string {
-    return this.gameService.gameState?.gameCode ?? '';
+  async copyLink() {
+    if (navigator.clipboard && window.isSecureContext) {
+      await navigator.clipboard.writeText(this.gameCodeLink);
+    }
+    else {
+      // Use the 'out of viewport hidden text area' trick
+      const textArea = document.createElement("textarea");
+      textArea.value = this.gameCodeLink;
+
+      // Move textarea out of the viewport so it's not visible
+      textArea.style.position = "absolute";
+      textArea.style.left = "-999999px";
+
+      document.body.prepend(textArea);
+      textArea.select();
+
+      try {
+        document.execCommand('copy');
+      } catch (error) {
+        console.error(error);
+      } finally {
+        textArea.remove();
+      }
+    }
+
+    this.gameCodeButtonText = this.gameService.gameCode + ' 👍';
+
+    setTimeout(() => {
+      this.gameCodeButtonText = this.gameService.gameCode + ' ✂';
+    }, 3000);
+
   }
 }
